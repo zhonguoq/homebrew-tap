10269cfd1ce6e208526aa65cecd728141670aa0c
zhonguoq <54052148+zhonguoq@users.noreply.github.com>
2025-03-24 12:14:06 +0800

    Update shs.rb

--- Formula/shs.rb
+++ Formula/shs.rb
@@ -1,8 +1,8 @@
 class Shs < Formula
   desc "A C library for packaging directories into a single file"
   homepage "https://github.com/zhonguoq/libshs"
-  url "https://github.com/zhonguoq/libshs/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e9628e15a620f39d06732b392188a57fb47af986cfa47feebe313fdc96d4a469"
+  url "https://github.com/zhonguoq/libshs/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "91c5d6685a69d459fa8d1525108254789e9371efdd9937499b423ba6aeb69a83"
   license "MIT"
 
   depends_on "cmake" => :build  # 如果使用 CMake 构建
